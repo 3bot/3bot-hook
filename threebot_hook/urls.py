a01db7545349c4b926d7cdd05042b56fdc484f4a
Walter Renner <renner@arteria.ch>
2017-05-26 16:28:51 +0200

    Django1.11 compatible urls

--- threebot_hook/urls.py
+++ threebot_hook/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, url, include
+# -*- coding: utf-8 -*-
+from django.conf.urls import url, include
 
-from .views import HookView, create, edit, hooks_list
+from threebot_hook import views
 
-urlpatterns = patterns('',
-    url(r'^list/(?P<wf_slug>[\w-]+)/$', hooks_list, name='hook_list'),
-    url(r'^create/(?P<wf_slug>[\w-]+)/$', create, name='hook_create'),
-    url(r'^edit/(?P<wf_slug>[\w-]+)/(?P<hook_slug>[\w-]+)/$', edit, name='hook_edit'),
-    url(r'^(?P<token>[\w-]+)/(?P<identifier>[\w-]+)/$', HookView.as_view()),
-)
+urlpatterns = [
+    url(r'^list/(?P<wf_slug>[\w-]+)/$', views.hooks_list, name='hook_list'),
+    url(r'^create/(?P<wf_slug>[\w-]+)/$', views.create, name='hook_create'),
+    url(r'^edit/(?P<wf_slug>[\w-]+)/(?P<hook_slug>[\w-]+)/$', views.edit, name='hook_edit'),
+    url(r'^(?P<token>[\w-]+)/(?P<identifier>[\w-]+)/$', views.HookView.as_view()),
+]
