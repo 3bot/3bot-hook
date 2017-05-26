fa5449dff2ee3acb869569f093aaa868e91311b4
Walter Renner <renner@arteria.ch>
2017-05-26 16:28:25 +0200

    added minimal test suite

---
+++ tests/urls.py
@@ -0,0 +1,13 @@
+"""URLs to run the tests."""
+from django.conf.urls import include, url
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^organizations/', include('organizations.urls')),
+    # url(r'^', include('threebot.urls')),
+    url(r'^', include('threebot_hook.urls')),
+]
